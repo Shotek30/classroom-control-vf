31b08337978c914cc3dae4611f38133e9de86cdf
Shotek30 <christopher.desmarteau@norseman.com>
2016-02-23 17:02:16 -0500

    Lab 11.1

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,17 @@
+class memcached {
+
+  package { 'memcached':
+   ensure => present,
+  }
+
+  file { '/etc/sysconfig/memcached':
+    ensure => file,
+    require => Package['memcached'],
+  }
+  
+  service { 'memcached':
+    ensure => running,
+    ensure => true,
+    subscribe => File['/etc/sysconfig/memcached'],
+  }
+}
