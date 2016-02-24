3f55ee9898f19d255c55d906196144a1a97645ba
Shotek30 <christopher.desmarteau@norseman.com>
2016-02-24 17:33:00 -0500

    Lab 15.1

---
+++ site/users/manifests/admins.pp
@@ -0,0 +1,16 @@
+class users::admins {
+
+  users::managed_users { 'jose': }
+  users::managed_users { 'alice': 
+    $group = 'devops',
+  }
+  
+  users::managed_users { 'chen':
+    $group = 'appdev',
+    $home_base = '/appdev',
+  }
+
+  file { "/appdev":
+    ensure => directory
+  }
+}
